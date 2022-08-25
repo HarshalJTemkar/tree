2a6f9110d6203d554dc31fbab0f2ddef838ba44d
HarshalJTemkar <49521616+HarshalJTemkar@users.noreply.github.com>
2022-08-25 16:36:53 +0530

    added icons
    added icons

--- tree/src/component/tree/tree.js
+++ tree/src/component/tree/tree.js
@@ -2,6 +2,8 @@ import * as React from "react";
 import TreeView from "@mui/lab/TreeView";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import FolderOpenIcon from '@mui/icons-material/FolderOpen';
+import FolderIcon from '@mui/icons-material/Folder';
 import TreeItem from "@mui/lab/TreeItem";
 import "../../ui/tree/tree.css";
 
@@ -29,6 +31,7 @@ class Tree extends React.Component {
       }
       return (
         <TreeItem
+          
           key={treeItemData.id}
           nodeId={treeItemData.id}
           label={treeItemData.name}
@@ -41,8 +44,9 @@ class Tree extends React.Component {
   DataTreeView = (treeItems) => {
     return (
       <TreeView
-        defaultCollapseIcon={<ExpandMoreIcon />}
-        defaultExpandIcon={<ChevronRightIcon />}
+        defaultCollapseIcon={<div>{<ExpandMoreIcon/>}{<FolderIcon/>}</div>}
+        defaultExpandIcon={<div><ChevronRightIcon/><FolderOpenIcon/></div>}
+        defaultEndIcon = {<FolderIcon/>}
         sx={{ height: 110, flexGrow: 1, maxWidth: 300 }}
       >
         {this.getTreeItemsFromData(treeItems)}
